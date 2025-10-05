32aeb1b4ca9e399d15d76fe61c9736142b23b302
AlexandreBnovais <157971881+AlexandreBnovais@users.noreply.github.com>
2025-10-05 00:29:51 -0300

    UPDATE: add shuttdown in the server file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import { createServer } from "http";
 import { app } from "./app/app.js";
+import 'dotenv/config';
 
 const server = createServer(app);
-const PORT = 3000;
-const HOST = "localhost";
+const PORT = process.env.APP_PORT || 3000;
+const HOST = process.env.APP_HOST;
 
 server.listen(PORT, () => {
     console.log(`Serve is running on http://${HOST}:${PORT} `);
+    console.log('Press CTRL + C to exit...');
 });
+
+process.on('SIGINT', () => {
+    console.log("server closing...");
+    server.close((err) => {
+        if(err) {
+            console.error('Error closing server', err);
+            process.exit(1);
+        }
+        console.log('Server closed');
+        process.exit(0);
+    });
+    
+})
